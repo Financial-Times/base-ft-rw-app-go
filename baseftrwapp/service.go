3e01f01cf144c3e3807bb7f8513e810b60404c80
Martin Garton <martin.garton@ft.com>
2016-07-11 14:53:29 +0100

    Use types from new shared API package
    The definition of Service has now been split out into its own package,
    to enable other code to depend on it without having to depend on the
    whole of this (rather complex) package.
    
    This is a step towards introducing an __ids endpoint

--- baseftrwapp/service.go
+++ baseftrwapp/service.go
@@ -1,16 +1,7 @@
 package baseftrwapp
 
 import (
-	"encoding/json"
+	"github.com/Financial-Times/up-rw-app-api-go/rwapi"
 )
 
-// Service defines the functions any read-write application needs to implement
-type Service interface {
-	Write(thing interface{}) error
-	Read(uuid string) (thing interface{}, found bool, err error)
-	Delete(uuid string) (found bool, err error)
-	DecodeJSON(*json.Decoder) (thing interface{}, identity string, err error)
-	Count() (int, error)
-	Check() error
-	Initialise() error
-}
+type Service rwapi.Service
